5c87fc832e54cb878212a277bf5d35acc1ea3337
Bansook Nam <bs.nam@lawfully.com>
2023-08-05 20:16:20 +0900

    사용하지 않는 logic 파일 제거

--- lib/screen/main/tab/todo/logic/todo.logic_interface.dart
+++
@@ -1,3 +0,0 @@
-import 'package:fast_app_base/screen/main/tab/todo/logic/todo_change_status.logic_interface.dart';
-
-abstract interface class TodoLogicInterface implements TodoChangeStatusLogicInterface {}
--- lib/screen/main/tab/todo/logic/todo_change_status.logic_interface.dart
+++
@@ -1,5 +0,0 @@
-import '../../../../../data/memory/vo_todo.dart';
-
-abstract interface class TodoChangeStatusLogicInterface {
-  void changeStatus(Todo todo);
-}
